a8c1f785ac971614e0e8a5c483ac34f31ded4434
airman66 <n89509176381@gmail.com>
2026-09-16 23:47:13 +0300

    feat: добавить Welcome к приветствию в feature/greeting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from utils import sum_even
 
 def greet(name: str) -> str:
     """Вернуть приветствие для имени."""
-    return f"Hello, {name}!"
+    return f"Hello, {name}! Welcome!"
 
 
 if __name__ == "__main__":
